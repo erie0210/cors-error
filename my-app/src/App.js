40f57ca4d2216820fc6f20e6a06589978ebdb1cb
erie0210 <2j00923@gmail.com>
2022-09-18 16:13:59 +0900

    init project

---
+++ my-app/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from "axios";
+import { useEffect,useState } from "react";
+
+function App() {
+  const [msg,setMsg] = useState("")
+
+  useEffect(()=>{
+    getHello()
+  },[])
+
+  const getHello = async () => {
+    const res = await axios.get('http://localhost:5000/api/v1')
+    setMsg(res.data)
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {msg}
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,4 @@
+### cors 에러 내기
+- my-app에서 $ yarn start
+- my-server에서 $ yarn start
+react 는 3000에서, nest는 5000에서 뜬다.
